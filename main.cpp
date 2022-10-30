d02aed5466b5538e4a7fa82773ff27b251c76caf
Popescu George <georgepopescu318@gmail.com>
2022-10-30 21:10:02 +0200

    v0.3 - fixing bugs

--- main.cpp
+++ main.cpp
@@ -162,23 +162,24 @@ std::ostream& operator<<(std::ostream& os,const game& gme){
 }
 
 void game::config(){
-    int inf_nr,arch_nr,cav_nr;
+    int inf_nr1 = 0,arch_nr1 = 0,cav_nr1 = 0;
     std::cout<<"   [FOR PLAYER 1] \n";
     std::cout<<"The number of infantry units will be:";
-    std::cin>>inf_nr;
+    std::cin>>inf_nr1;
     std::cout<<"The number of archers units will be:";
-    std::cin>>arch_nr;
+    std::cin>>arch_nr1;
     std::cout<<"The number of cavalry units will be:";
-    std::cin>>cav_nr;
-    p1.init(inf_nr,arch_nr,cav_nr);
+    std::cin>>cav_nr1;
+    p1.init(inf_nr1,arch_nr1,cav_nr1);
+    int inf_nr2 = 0,arch_nr2 = 0,cav_nr2 = 0;
     std::cout<<"\n   [FOR PLAYER 2] \n";
     std::cout<<"The number of infantry units will be:";
-    std::cin>>inf_nr;
+    std::cin>>inf_nr2;
     std::cout<<"The number of archers units will be:";
-    std::cin>>arch_nr;
+    std::cin>>arch_nr2;
     std::cout<<"The number of cavalry units will be:";
-    std::cin>>cav_nr;
-    p2.init(inf_nr,arch_nr,cav_nr);
+    std::cin>>cav_nr2;
+    p2.init(inf_nr1,arch_nr1,cav_nr1);
 }
 void game::start_game(const game& gme) {
     config();
