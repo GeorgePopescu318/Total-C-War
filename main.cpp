71cf8559dd7d9988c87744a69586ca42278f92d9
Popescu George <georgepopescu318@gmail.com>
2022-11-14 13:13:57 +0200

    v0.5 - fixed

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
-#include <iostream>
-#include <vector>
 /// {infantry, archers, cavalry};
 #include "headers/game.hpp"
 int main() {
     game g1;
-   g1.start_game();
+    g1.game::start_game();
     return 0;
 }
 
