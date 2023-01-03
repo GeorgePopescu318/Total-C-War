aa0ae8da81caf8fa2354cde258b96109e9f93b4c
Popescu George <georgepopescu318@gmail.com>
2023-01-03 18:04:09 +0200

    v0.12 - added charge bonus

--- headers/archers.hpp
+++ headers/archers.hpp
@@ -14,8 +14,8 @@ public:
     archers() = default;
     archers(int player_, int x_, int y_);
     archers(const int health_, const int dmg_, const int defence_, const int range_, const int movement_pts_,const int miss_chance_,
-            const int x_, const int y_, const int player_)
-            : unit(health_,dmg_,defence_,range_,movement_pts_,x_,y_,player_), miss_chance{miss_chance_}{}
+            const int x_, const int y_, const int player_, unit* enemy_, bool in_range_)
+            : unit(health_,dmg_,defence_,range_,movement_pts_,x_,y_,player_), miss_chance{miss_chance_}, enemy{enemy_}, in_range{in_range_}{}
     friend std::ostream& operator<<(std::ostream& os,const archers& arch);
     archers(const archers& other) : unit(other), miss_chance{other.miss_chance}{}
     void print_value() override{
--- headers/cavalry.hpp
+++ headers/cavalry.hpp
@@ -13,8 +13,8 @@ public:
     cavalry() =default;
     cavalry(int player_, int x_, int y_);
     cavalry(const int health_, const int dmg_, const int defence_, const int range_, const int movement_pts_,
-            const int charge_bonus_,const int x_, const int y_, const int player_)
-            : unit(health_,dmg_,defence_,range_,movement_pts_,x_,y_,player_), charge_bonus{charge_bonus_}{}
+            const int charge_bonus_,const int x_, const int y_, const int player_,const int old_x_, const int old_y_)
+            : unit(health_,dmg_,defence_,range_,movement_pts_,x_,y_,player_), charge_bonus{charge_bonus_}, old_x{old_x_}, old_y{old_y_}{}
     friend std::ostream& operator<<(std::ostream& os,const cavalry& cav);
     cavalry(const cavalry& other) : unit(other), charge_bonus{other.charge_bonus}{}
     void print_value() override{
